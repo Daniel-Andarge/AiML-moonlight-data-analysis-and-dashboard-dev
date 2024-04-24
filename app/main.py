265cb4e3dedc814dd1c9c83afeb47a851779ca92
Daniel-Andarge <andargedaniel90@gmail.com>
2024-04-24 22:10:29 +0300

    removing hard coded csv data

--- app/main.py
+++ app/main.py
@@ -11,10 +11,7 @@ st.sidebar.header("Data Upload")
 uploaded_file = st.sidebar.file_uploader("Upload CSV file", type="csv")
 
 # Load the solar radiation measurement data
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-else:
-    data = pd.read_csv("cleaned_sierraleon_dataset.csv")
+data = pd.read_csv(uploaded_file) if uploaded_file is not None else None
 
 # Add a separator in the sidebar
 st.sidebar.markdown("---")
@@ -42,24 +39,26 @@ elif selected_methodology == "Regression Analysis":
 # Visualize data insights
 st.header("Data Insights")
 
-# Time-series plot of GHI
-st.subheader("Time-Series Plot of Global Horizontal Irradiance (GHI)")
-plt.plot(data["Timestamp"], data["GHI"])
-plt.xlabel("Timestamp")
-plt.ylabel("GHI (W/m²)")
-plt.title("Global Horizontal Irradiance (GHI) Over Time")
-st.pyplot(plt)
+if data is not None:
+    # Time-series plot of GHI
+    st.subheader("Time-Series Plot of Global Horizontal Irradiance (GHI)")
+    plt.plot(data["Timestamp"], data["GHI"])
+    plt.xlabel("Timestamp")
+    plt.ylabel("GHI (W/m²)")
+    plt.title("Global Horizontal Irradiance (GHI) Over Time")
+    st.pyplot(plt)
 
-# Scatter plot of GHI vs. Ambient Temperature
-st.subheader("Scatter Plot of GHI vs. Ambient Temperature")
-plt.scatter(data["GHI"], data["Tamb"])
-plt.xlabel("GHI (W/m²)")
-plt.ylabel("Ambient Temperature (°C)")
-plt.title("GHI vs. Ambient Temperature")
-st.pyplot(plt)
+    # Scatter plot of GHI vs. Ambient Temperature
+    st.subheader("Scatter Plot of GHI vs. Ambient Temperature")
+    plt.scatter(data["GHI"], data["Tamb"])
+    plt.xlabel("GHI (W/m²)")
+    plt.ylabel("Ambient Temperature (°C)")
+    plt.title("GHI vs. Ambient Temperature")
+    st.pyplot(plt)
 
 
 
-# Display the cleaned data table
-st.subheader("Solar Radiation Measurement Data")
-st.dataframe(data)
+# Display the cleaned data table if data is available
+if data is not None:
+    st.subheader("Solar Radiation Measurement Data")
+    st.dataframe(data)
